6dbcf357b7d0a2ef2aa39a2cf1231e72e34e4fb1
Juarez Lopes <thedevelopes@gmail.com>
2017-05-14 13:28:20 -0300

    Add initial dynamic routing and create fixture test folder
    This reverts commit 2f2b3281f44f2f97a19085f4ab378e9a8d1244fb.

--- imports/ui/Note_List_Item.test.js
+++ imports/ui/Note_List_Item.test.js
@@ -2,26 +2,40 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import expect from 'expect'
 import { mount } from 'enzyme'
-import NoteListItem from './Note_List_Item'
-
+import { NoteListItem } from './Note_List_Item'
+import { notes } from '../fixtures/fixtures'
 
 if (Meteor.isClient) {
+  let Session
+
+  beforeEach(() => {
+    Session = {
+      set: expect.createSpy()
+    }
+  })
+
+
   describe('NoteListItem', function () {
     it('should set title and timestamp', function () {
-      const title = 'My test title'
-      const updatedAt = 1494700222936
-      const wrapper = mount(<NoteListItem note={{title, updatedAt}}/>)
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>)
 
-      expect(wrapper.find('h5').text()).toBe(title)
+      expect(wrapper.find('h5').text()).toBe(notes[0].title)
       expect(wrapper.find('p').text()).toBe('May 13, 2017')
 
     })
 
     it('should set the default undefined title if not provided', function () {
-      const title = ''
-      const wrapper = mount(<NoteListItem note={{ title }} />)
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>)
 
       expect(wrapper.find('h5').text()).toBe('Untitled')
+    })
+
+    it('should call set on click', function () {
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>)
+
+      wrapper.find('div').simulate('click')
+
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id)
     })
   })
 }
